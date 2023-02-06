632eb25f40daf213121c22319ed9b158b4bf697f
arnobmukherjee1988 <arnobmukherjee1988@gmail.com>
2023-02-06 18:40:12 -0500

    Create simple_linear_regression.py

---
+++ Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Simple Linear Regression
+
+# <a name="4"></a>
+# ## Concept
+# 
+# In this practice lab, you will fit the linear regression parameters $(w,b)$ to your dataset.
+# - The model function for linear regression, which is a function that maps from `x` (YearsExperience) to `y` (Salary) is represented as 
+#     $$f_{w,b}(x) = wx + b$$
+#     
+# 
+# - To train a linear regression model, you want to find the best $(w,b)$ parameters that fit your dataset.  
+# 
+#     - To compare how one choice of $(w,b)$ is better or worse than another choice, you can evaluate it with a cost function $J(w,b)$
+#       - $J$ is a function of $(w,b)$. That is, the value of the cost $J(w,b)$ depends on the value of $(w,b)$.
+#   
+#     - The choice of $(w,b)$ that fits your data the best is the one that has the smallest cost $J(w,b)$.
+# 
+# 
+# - To find the values $(w,b)$ that gets the smallest possible cost $J(w,b)$, you can use a method called **gradient descent**. 
+#   - With each step of gradient descent, your parameters $(w,b)$ come closer to the optimal values that will achieve the lowest cost $J(w,b)$.
+#   
+# 
+# - The trained linear regression model can then take the input feature $x$ (city population) and output a prediction $f_{w,b}(x)$ (predicted monthly profit for a restaurant in that city).
+
+# ## 2 -  Problem Statement
+# 
+# Suppose you are the Manager of a Small firm and are considering hiring new recruitments.
+# - You would like to hire best possible candidate based on experience and determine the salary for the candidate.
+# - The firm already has few employees with varying job experience, and the Manager has salatry data for those employees.
+#     
+# Can you use the data to select the best candidates?
+
+# ## Importing the libraries
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# %matplotlib notebook
+
+
+# ## Importing the dataset
+
+# In[2]:
+
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+dataset.head()
+
+
+# In[3]:
+
+
+print(X)
+
+
+# In[4]:
+
+
+print(y)
+
+
+# ## Splitting the dataset into the Training set and Test set
+
+# In[5]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X1_train, X1_test, y1_train, y1_test = X_train, X_test, y_train, y_test
+
+
+# ## Training the Simple Linear Regression model on the Training set
+
+# In[14]:
+
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+# ## Predicting the Test set results
+
+# In[15]:
+
+
+y_pred = regressor.predict(X_test)
+
+
+# ## Visualising the Training set results
+
+# In[16]:
+
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# ## Visualising the Test set results
+
+# In[18]:
+
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# ## To test the Gradient descent method of scikit-learn linear model 
+
+# In[10]:
+
+
+from sklearn.linear_model import SGDRegressor
+clf_ = SGDRegressor()
+clf_.fit(X1_train, y1_train)
+plt.grid()
+plt.scatter(X1_train,y1_train, c='r')
+plt.plot(X1_train,clf_.predict(X1_train), c='b')
+
+
+# In[11]:
+
+
+y1_pred = clf_.predict(X1_test)
+
+
+# In[12]:
+
+
+plt.scatter(X1_train, y1_train, color = 'red')
+plt.plot(X1_train, clf_.predict(X1_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# In[13]:
+
+
+plt.scatter(X1_test, y1_test, color = 'red')
+plt.plot(X1_train, clf_.predict(X1_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
